4a95f3e17769c6c23664a7d5ac5840b4d8b74e04
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-05 22:05:09 +0200

    app

--- node/task3/app.js
+++ node/task3/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); // check more info about .env and .env.prod
 
 const { mainRouter } = require('./api/api.router');
 const { PORT, URI } = require('./configs/variables');
+const { ApiError } = require('./errors/apiError');
 
 const app = express();
 
@@ -20,7 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', mainRouter);
+app.use('/*', _notFoundError);
+app.use(mainErrorHandler);
 
 app.listen(PORT, () => {    
     console.log(`App listen http://localhost:${PORT} port.`);
 });
+
+function _notFoundError(req, res, next) {
+    next(new ApiError('Route not found!', 404));
+}
+
+function mainErrorHandler(err, req, res, next) {
+    res
+    .status(err.status || 500)
+    .json({
+        message: err.message || 'Unknown error'
+    });
+}
