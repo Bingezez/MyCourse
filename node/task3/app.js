c3d7f431268191c710a8ce1bd6e7eb6d7feba68b
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-06 17:53:12 +0200

    app

--- node/task3/app.js
+++ node/task3/app.js
@@ -2,29 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); // check more info about .env and .env.prod (other .env files)
 
-const { mainRouter } = require('./api/api.router');
-const { PORT, URI } = require('./configs/variables');
-const { ApiError } = require('./errors/apiError');
+const {mainRouter} = require('./api/api.router');
+const {PORT, URI} = require('./configs/variables');
+const {ApiError} = require('./errors/apiError');
 
 const app = express();
 
 // mongoose.set('debug', true);
 mongoose.set('strictQuery', true);
 mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true, useUnifiedTopology: true
 })
-.then(() => {console.log('Connected!')})
-.catch((e) => {console.log(e)});
+    .then(() => {
+        console.log('Connected!');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', mainRouter);
 app.use('/*', _notFoundError);
 app.use(mainErrorHandler);
 
-app.listen(PORT, () => {    
+app.listen(PORT, () => {
     console.log(`App listen http://localhost:${PORT} port.`);
 });
 
@@ -34,8 +37,8 @@ function _notFoundError(req, res, next) {
 
 function mainErrorHandler(err, req, res, next) {
     res
-    .status(err.status || 500)
-    .json({
-        message: err.message || 'Unknown error'
-    });
+        .status(err.status || 500)
+        .json({
+            message: err.message || 'Unknown error'
+        });
 }
