40aad9fcdc244ec9d32ff04f6ac0518305ead1f7
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-06 17:54:29 +0200

    var

--- node/task3/configs/variables.js
+++ node/task3/configs/variables.js
@@ -6,4 +6,4 @@ const dbName = 'database';
 module.exports = {
     PORT: process.env.PORT || 5050,
     URI: process.env.URI || `mongodb+srv://${username}:${password}@${cluster}.jwt5ndr.mongodb.net/${dbName}?retryWrites=true&w=majority`
-}
+};
