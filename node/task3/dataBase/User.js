20747b82a88776683b1f1b06feba9ed6a2515098
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-06 17:54:01 +0200

    mongoose database

--- node/task3/dataBase/User.js
+++ node/task3/dataBase/User.js
@@ -2,46 +2,23 @@ const mongoose = require('mongoose');
 const rolesEnum = require('../configs/roles.enum');
 
 const UserScheme = new mongoose.Schema({
-        fisrtName: {
-            type: String,
-            trim: true,
-            default: ''
-        },
-        lastName: {
-            type: String,
-            trim: true,
-            default: ''
-        },
-        username: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true
-        },
-        email: {
-            type: String,
-            trim: true,
-            lowercase: true,
-            required: true,
-            unique: true
-        },
-        age: {
-            type: Number
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        role: {
-            type: String,
-            enum: Object.values(rolesEnum),
-            default: rolesEnum.USER
-         }    
-    }, 
-    {
-        timestamps: true,
-        versionKey: false
+    fisrtName: {
+        type: String, trim: true, default: ''
+    }, lastName: {
+        type: String, trim: true, default: ''
+    }, username: {
+        type: String, trim: true, required: true, unique: true
+    }, email: {
+        type: String, trim: true, lowercase: true, required: true, unique: true
+    }, age: {
+        type: Number
+    }, password: {
+        type: String, required: true
+    }, role: {
+        type: String, enum: Object.values(rolesEnum), default: rolesEnum.USER
     }
-);  
+}, {
+    timestamps: true, versionKey: false
+});
 
 module.exports = mongoose.model('User', UserScheme);
