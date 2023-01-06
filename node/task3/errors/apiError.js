19f3eee9a22ac697d4f1d899b62863118402f45b
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-06 17:54:16 +0200

    error

--- node/task3/errors/apiError.js
+++ node/task3/errors/apiError.js
@@ -1,26 +1,24 @@
 class ApiError extends Error {
-    constructor (message, status) {
+    constructor(message, status) {
         super(message);
         this.status = status;
     }
 }
 
 class NotFoundError extends Error {
-    constructor (message) {
+    constructor(message) {
         super(message);
         this.status = 404;
     }
 }
 
 class BadRequestError extends Error {
-    constructor (message) {
+    constructor(message) {
         super(message);
         this.status = 400;
     }
 }
 
 module.exports = {
-    ApiError,
-    NotFoundError,
-    BadRequestError
+    ApiError, NotFoundError, BadRequestError
 };
