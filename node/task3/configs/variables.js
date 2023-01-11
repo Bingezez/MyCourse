d3af8013b1bb596b57e13c0dbcd3188e2ec4993f
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-11 14:59:46 +0200

    var

--- node/task3/configs/variables.js
+++ node/task3/configs/variables.js
@@ -1,9 +1,4 @@
-const username = 'admin';
-const password = 'admin';
-const cluster = 'cluster0';
-const dbName = 'database';
-
 module.exports = {
     PORT: process.env.PORT || 5051,
-    URL: process.env.URL || `mongodb+srv://${username}:${password}@${cluster}.jwt5ndr.mongodb.net/${dbName}?retryWrites=true&w=majority`
+    URL: process.env.URL
 };
