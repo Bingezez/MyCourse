ed7a8b8a1e99c6090bfabe2ed5b4afaab56894a6
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-20 19:25:06 +0200

    error

--- node/lectionOauth/errors/apiError.js
+++ node/lectionOauth/errors/apiError.js
@@ -1,3 +1,5 @@
+const code = require('./error.codes');
+
 class ApiError extends Error {
     constructor(message, status) {
         super(message);
@@ -8,17 +10,51 @@ class ApiError extends Error {
 class NotFoundError extends Error {
     constructor(message) {
         super(message);
-        this.status = 404;
+        this.status = code.NOT_FOUND;
     }
 }
 
 class BadRequestError extends Error {
     constructor(message) {
         super(message);
-        this.status = 400;
+        this.status = code.BAD_REQUEST;
+    }
+}
+
+class UnauthorizedError extends Error {
+    constructor(message) {
+        super(message);
+        this.status = code.UNAUTHORIZED;
+    }
+}
+
+class ForbiddenError extends Error {
+    constructor(message) {
+        super(message);
+        this.status = code.FORBIDDEN;
+    }
+}
+
+class ConflictError extends Error {
+    constructor(message) {
+        super(message);
+        this.status = code.FORBIDDEN;
+    }
+}
+
+class ServerError extends Error {
+    constructor(message) {
+        super(message);
+        this.status = code.SERVER_ERROR;
     }
 }
 
 module.exports = {
-    ApiError, NotFoundError, BadRequestError
+    ApiError,
+    NotFoundError,
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    ConflictError,
+    ServerError
 };
