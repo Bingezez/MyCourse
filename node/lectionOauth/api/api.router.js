4b15107f8be8cec18755e65e8c497077a188707f
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-01-20 19:26:06 +0200

    task4

--- node/lectionOauth/api/api.router.js
+++ node/lectionOauth/api/api.router.js
@@ -1,6 +1,9 @@
 const mainRouter = require('express').Router();
-const {userRouter} = require('./user/user.router');
 
+const {userRouter} = require('./user/user.router');
+const {authRouter} = require('./auth/auth.router');
+
+mainRouter.use('/auth', authRouter);
 mainRouter.use('/users', userRouter);
 
 module.exports = {
