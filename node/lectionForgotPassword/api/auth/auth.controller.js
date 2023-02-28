a316633063ae7eed0798e86624fc382c60aa7fe4
Ruslan Seldiukov <50172150+Bingezez@users.noreply.github.com>
2023-02-28 11:57:57 +0200

    lection forgot password

---
+++ node/lectionForgotPassword/api/auth/auth.controller.js
@@ -0,0 +1,59 @@
+const service = require('./auth.service');
+const {oauthService, emailService } = require('../../services');
+const { NO_CONTENT } = require('../../errors/error.codes');
+const { BANNED } = require('../../configs/emailTypes.enum');
+
+module.exports = {
+    loginUser: async (req, res, next) => {
+        try {
+            const user = req.locals.user; 
+
+            emailService.sendSGMail('072001rusand@gmail.com', BANNED);
+            await oauthService.checkHashPassword(user.password, req.body.password);
+
+            const tokenPair = oauthService.generateAccessTokenPair({ ...user });
+            
+            await service.createOauthPair({...tokenPair, _userId: user._id});
+            res.json({...tokenPair, _userId: user._id});
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    refreshUser: async (req, res, next) => {
+        try {
+            const user = req.user; 
+            const refreshToken = req.get('Authorization');
+
+            await service.deleteOneByParams({refreshToken});
+
+            const newTokenPair = oauthService.generateAccessTokenPair({ ...user });
+            
+            await service.createOauthPair({...newTokenPair, _userId: user._id});
+            res.json({...newTokenPair, _userId: user._id});
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    logoutUser: async (req, res, next) => {
+        try {
+            const accessToken = req.get('Authorization');
+            await service.deleteOneByParams({accessToken});
+            
+            res.status(NO_CONTENT).send('Logout');
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    logoutUserMany: async (req, res, next) => {
+        try {
+            await service.deleteManyByParams({user: req.user._id});
+
+            res.status(NO_CONTENT).send('Logout all');
+        } catch (e) {
+            next(e);
+        }
+    }
+};
---
+++ node/lectionForgotPassword/services/oauth.services.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { BadRequestError, UnauthorizedError } = require('../errors/apiError');
+const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = require('../configs/variables');
+
+const hashPassword = async (password, saltRound = 10) => bcrypt.hash(password, saltRound);
+
+const checkHashPassword = async (password, hashPassword) => {
+    const isPasswordEquals = bcrypt.compare(password, hashPassword);
+
+    if (!isPasswordEquals.valueOf()) {
+        throw new BadRequestError('Email or password is wrong');
+    }
+};  
+
+const generateAccessTokenPair = (encodeData = {}) => {
+    const accessToken = jwt.sign(encodeData, ACCESS_TOKEN_SECRET, {expiresIn: '15m'});
+    const refreshToken = jwt.sign(encodeData, REFRESH_TOKEN_SECRET, {expiresIn: '30d'});
+
+    return {
+        accessToken,
+        refreshToken
+    };
+};
+
+const validateTokenDynemically = (tokenType = '', token = '') => {
+    try {
+        const type = tokenType === 'accessToken' ? ACCESS_TOKEN_SECRET: REFRESH_TOKEN_SECRET;
+
+        return jwt.verify(token, type);
+    } catch (e) {
+        throw new UnauthorizedError(e.message || 'Invalide Token.');
+    }
+};
+
+module.exports = {
+    hashPassword,
+    checkHashPassword,
+    generateAccessTokenPair,
+    validateTokenDynemically
+};
